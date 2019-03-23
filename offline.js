61ee3a1249d1c8094b020be65dad247e2dd65139
Luffy3090 <Luffy3090@users.noreply.github.com>
2019-03-23 18:21:56 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1553352152,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/map1.png",
+		"images/map2.png",
+		"images/block-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/end-sheet0.png",
+		"images/turrent-sheet0.png",
+		"images/attack_bs-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/turrenteffict-sheet0.png",
+		"images/towerred-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
